57e9dce021ea52b0e2c599e99d860fcb463fb664
Sambir <124112301+sambirsb@users.noreply.github.com>
2025-09-11 14:33:49 +0200

    Icon component

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,19 +1,20 @@
-import type { FC } from 'react'
+import type { ComponentProps, FC } from 'react'
 
-interface Props {
-  name: IconName;
-}
+type Props = {
+  name: IconName,
+} & ComponentProps<'svg'>
 
 type IconName = keyof typeof icons
 
 const icons = {
-  trash: (props: { className?: string }) => (
+  trash: (props: ComponentProps<'svg'>) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="18"
       height="18"
       viewBox="0 0 18 18"
       fill="none"
+      {...props}
     >
       <path
         d="M3.87414 7.61505C3.80712 6.74386 4.49595 6 5.36971 6H12.63C13.5039 6 14.1927 6.74385 14.1257 7.61505L13.6064 14.365C13.5463 15.1465 12.8946 15.75 12.1108 15.75H5.88894C5.10514 15.75 4.45348 15.1465 4.39336 14.365L3.87414 7.61505Z"
@@ -28,13 +29,14 @@ const icons = {
       <path d="M7.5 9V12.75" stroke="#CDCDCD" strokeLinecap="round" />
     </svg>
   ),
-  frame: () => (
+  frame: (props: ComponentProps<'svg'>) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="18"
       height="18"
       viewBox="0 0 18 18"
       fill="none"
+      {...props}
     >
       <path
         d="M8.67272 5.99106L2 12.6637V16H5.33636L12.0091 9.32736M8.67272 5.99106L11.0654 3.59837L11.0669 3.59695C11.3962 3.26759 11.5612 3.10261 11.7514 3.04082C11.9189 2.98639 12.0993 2.98639 12.2669 3.04082C12.4569 3.10257 12.6217 3.26735 12.9506 3.59625L14.4018 5.04738C14.7321 5.37769 14.8973 5.54292 14.9592 5.73337C15.0136 5.90088 15.0136 6.08133 14.9592 6.24885C14.8974 6.43916 14.7324 6.60414 14.4025 6.93398L14.4018 6.93468L12.0091 9.32736M8.67272 5.99106L12.0091 9.32736"
@@ -44,7 +46,7 @@ const icons = {
       />
     </svg>
   ),
-  loader: () => (
+  loader: (props: ComponentProps<'svg'>) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="24"
@@ -55,14 +57,15 @@ const icons = {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
+      {...props}
     >
       <path d="M21 12a9 9 0 1 1-6.219-8.56" />
     </svg>
   ),
 }
 
-export const Icon: FC<Props> = (props) => {
-  return icons[props.name]
-}
+export const Icon: FC<Props> = ({ name, ...rest }) => {
+  const Component = icons[name]
 
-// обєкт айконс кліч строка а значеня функціональна компонента
+  return <Component {...rest} />
+}
